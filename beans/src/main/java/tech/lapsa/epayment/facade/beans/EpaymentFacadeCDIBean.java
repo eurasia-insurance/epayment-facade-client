62095fc7e841553fe028a4d942d729edab35d0cc
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-12-03 13:31:11 +0600

    cdi beans

---
+++ beans/src/main/java/tech/lapsa/epayment/facade/beans/EpaymentFacadeCDIBean.java
@@ -0,0 +1,48 @@
+package tech.lapsa.epayment.facade.beans;
+
+import java.net.URI;
+
+import javax.ejb.EJB;
+import javax.enterprise.context.Dependent;
+
+import tech.lapsa.epayment.domain.Invoice;
+import tech.lapsa.epayment.domain.Invoice.InvoiceBuilder;
+import tech.lapsa.epayment.domain.Payment;
+import tech.lapsa.epayment.facade.CDItoEJBDelegate;
+import tech.lapsa.epayment.facade.EpaymentFacade;
+import tech.lapsa.epayment.facade.InvoiceNotFound;
+import tech.lapsa.java.commons.function.MyExceptions.IllegalArgument;
+import tech.lapsa.java.commons.function.MyExceptions.IllegalState;
+
+@Dependent
+@CDItoEJBDelegate
+public class EpaymentFacadeCDIBean implements EpaymentFacade {
+
+    @EJB
+    private EpaymentFacade delegate;
+
+    @Override
+    public URI getDefaultPaymentURI(final Invoice invoice) throws IllegalArgument, IllegalState {
+	return delegate.getDefaultPaymentURI(invoice);
+    }
+
+    @Override
+    public Invoice accept(final Invoice invoice) throws IllegalArgument, IllegalState {
+	return delegate.accept(invoice);
+    }
+
+    @Override
+    public Invoice completeAndAccept(final InvoiceBuilder invoiceBuilder) throws IllegalArgument, IllegalState {
+	return delegate.completeAndAccept(invoiceBuilder);
+    }
+
+    @Override
+    public Invoice invoiceByNumber(final String number) throws IllegalArgument, IllegalState, InvoiceNotFound {
+	return delegate.invoiceByNumber(number);
+    }
+
+    @Override
+    public void invoiceHasPaidBy(final Invoice invoice, final Payment payment) throws IllegalArgument, IllegalState {
+	delegate.invoiceHasPaidBy(invoice, payment);
+    }
+}
