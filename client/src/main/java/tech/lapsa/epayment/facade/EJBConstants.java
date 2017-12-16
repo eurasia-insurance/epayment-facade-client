8b5bf5659f1462f9e937f56b095bd4883beb0914
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-12-16 10:12:16 +0600

    #cfrefact

--- client/src/main/java/tech/lapsa/epayment/facade/EJBConstants.java
+++ client/src/main/java/tech/lapsa/epayment/facade/EJBConstants.java
@@ -2,6 +2,6 @@ package tech.lapsa.epayment.facade;
 
 public interface EJBConstants {
 
-    public static final String APPLICATION_NAME = "epayment-facade-ear";
+    public static final String REMOTE_APP_NAME = "epayment-facade-app";
     public static final String MODULE_NAME = "epayment-facade";
 }
